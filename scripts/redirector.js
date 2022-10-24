1f04b93095139e2f54c001aa3699ed9efda69c6d
unknown <kymohendriks@gmail.com>
2022-10-24 23:11:44 +0200

    added redirect links to different pages

---
+++ scripts/redirector.js
@@ -0,0 +1,16 @@
+function redirectTo(page) {
+    switch(page) {
+        case "mcmods":
+            window.location.href = "https://superpro148.github.io/pages/mcmods.html";
+            break;
+        case "cookieclicker":
+            window.location.href = "https://superpro148.github.io/pages/cookieclicker.html";
+            break;
+        case "other":
+            window.location.href = "https://superpro148.github.io/pages/other.html";
+            break;
+        case "github":
+            window.location.href = "https://github.com/SuperPro148";
+            break;
+    }
+}
