363472ec096bf676dd8bcbf5af571cf4331c28a9
unknown <kymohendriks@gmail.com>
2022-12-04 23:02:38 +0100

    full redesign

---
+++ script.js
@@ -0,0 +1,76 @@
+function openMinecraftMods() {
+    document.querySelectorAll(".shown").forEach((i) => {
+        i.classList.remove("shown");
+        i.classList.add("hidden");
+    });
+    let mcModsDiv = document.getElementById("mc-mods-div");
+    if (mcModsDiv.hasChildNodes()) {
+        while (mcModsDiv.hasChildNodes()) {
+            mcModsDiv.removeChild(mcModsDiv.lastChild);
+        }
+    }
+    setTimeout(() => {
+        createModElement("quickarmorswap");
+        setTimeout(() => {
+            createModElement("armorhud-fabric");
+            setTimeout(() => {
+                createModElement("platformprecision");
+                setTimeout(() => {
+                    createModElement("configlib148");
+                    setTimeout(() => {
+                        createModElement("supers-test-mod");
+                        setTimeout(() => {
+                            mcModsDiv.classList.remove("hidden");
+                            mcModsDiv.classList.add("shown");
+                        }, 200);
+                    }, 200);
+                }, 200);
+            }, 200);
+        }, 200);
+    }, 200);
+}
+
+function createModElement(slug) {
+    function modrinthApiResponse() {
+        let apiResponse = JSON.parse(this.response);
+        
+        let mainElem = document.createElement("div");
+        mainElem.classList.add("page-element");
+        mainElem.classList.add("mc-mod");
+
+        let iconElem = document.createElement("img");
+        iconElem.classList.add("mc-mod-icon");
+        iconElem.src = `img/mc_mods/${slug}.png`;
+
+        let iconLinkElem = document.createElement("a");
+        iconLinkElem.href = `https://modrinth.com/mod/${slug}`
+        iconLinkElem.appendChild(iconElem);
+        mainElem.appendChild(iconLinkElem);
+
+        let titleElem = document.createElement("p");
+        titleElem.classList.add("mc-mod-title");
+        titleElem.innerHTML = apiResponse.title;
+        mainElem.appendChild(titleElem);
+
+        let descElem = document.createElement("p");
+        descElem.classList.add("mc-mod-desc");
+        descElem.innerHTML = apiResponse.description;
+        mainElem.appendChild(descElem);
+
+        let downloadIconElem = document.createElement("img");
+        downloadIconElem.classList.add("mc-mod-download-icon");
+        downloadIconElem.src = "img/feather_icons/download.svg";
+        mainElem.appendChild(downloadIconElem);
+
+        let downloadCountElem = document.createElement("p");
+        downloadCountElem.classList.add("mc-mod-download-count");
+        downloadCountElem.innerHTML = apiResponse.downloads;
+        mainElem.appendChild(downloadCountElem);
+    
+        document.getElementById("mc-mods-div").appendChild(mainElem);
+    }
+    let xhttp = new XMLHttpRequest();
+    xhttp.addEventListener("load", modrinthApiResponse)
+    xhttp.open("GET", `https://api.modrinth.com/v2/project/${slug}`);
+    xhttp.send();
+}
