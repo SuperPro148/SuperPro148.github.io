24aba6c68d86bb1ae501c64fb5f8e8f374192ac2
unknown <kymohendriks@gmail.com>
2022-12-18 20:16:20 +0100

    replaced xmlhttprequests with fetch()

--- script.js
+++ script.js
@@ -39,51 +39,45 @@ function openMinecraftMods() {
 }
 
 async function createModElement(slug, order) {
-    function modrinthApiResponse() {
-        let apiResponse = JSON.parse(this.response);
+    let apiResponse = await (await fetch(`https://api.modrinth.com/v2/project/${slug}`)).json();
         
-        let mainElem = document.createElement("div");
-        mainElem.classList.add("page-element");
-        mainElem.classList.add("mc-mod");
+    let mainElem = document.createElement("div");
+    mainElem.classList.add("page-element");
+    mainElem.classList.add("mc-mod");
 
-        let iconElem = document.createElement("img");
-        iconElem.classList.add("mc-mod-icon");
-        iconElem.src = `img/mc_mods/${slug}.png`;
-        iconElem.setAttribute("onclick", `window.open('https://modrinth.com/mod/${slug}')`);
-        mainElem.appendChild(iconElem);
+    let iconElem = document.createElement("img");
+    iconElem.classList.add("mc-mod-icon");
+    iconElem.src = `img/mc_mods/${slug}.png`;
+    iconElem.setAttribute("onclick", `window.open('https://modrinth.com/mod/${slug}')`);
+    mainElem.appendChild(iconElem);
 
-        let titleElem = document.createElement("p");
-        titleElem.classList.add("mc-mod-title");
-        titleElem.innerHTML = apiResponse.title;
-        mainElem.appendChild(titleElem);
+    let titleElem = document.createElement("p");
+    titleElem.classList.add("mc-mod-title");
+    titleElem.innerHTML = apiResponse.title;
+    mainElem.appendChild(titleElem);
 
-        let descElem = document.createElement("p");
-        descElem.classList.add("mc-mod-desc");
-        descElem.innerHTML = apiResponse.description;
-        mainElem.appendChild(descElem);
+    let descElem = document.createElement("p");
+    descElem.classList.add("mc-mod-desc");
+    descElem.innerHTML = apiResponse.description;
+    mainElem.appendChild(descElem);
 
-        let downloadIconElem = document.createElement("img");
-        downloadIconElem.classList.add("mc-mod-download-icon");
-        downloadIconElem.src = "img/lucide/download.svg";
-        mainElem.appendChild(downloadIconElem);
+    let downloadIconElem = document.createElement("img");
+    downloadIconElem.classList.add("mc-mod-download-icon");
+    downloadIconElem.src = "img/lucide/download.svg";
+    mainElem.appendChild(downloadIconElem);
 
-        let downloadCountElem = document.createElement("p");
-        downloadCountElem.classList.add("mc-mod-download-count");
-        downloadCountElem.innerHTML = apiResponse.downloads;
-        mainElem.appendChild(downloadCountElem);
+    let downloadCountElem = document.createElement("p");
+    downloadCountElem.classList.add("mc-mod-download-count");
+    downloadCountElem.innerHTML = apiResponse.downloads;
+    mainElem.appendChild(downloadCountElem);
     
-        modElements[order] = mainElem;
-        modElementsFinished.push(true);
-        if (modElementsFinished.length === 5) {
-            modElements.forEach((elem) => {
-                document.getElementById("mc-mods-div").appendChild(elem);
-            });
-        }
+    modElements[order] = mainElem;
+    modElementsFinished.push(true);
+    if (modElementsFinished.length === 5) {
+        modElements.forEach((elem) => {
+            document.getElementById("mc-mods-div").appendChild(elem);
+        });
     }
-    let xhttp = new XMLHttpRequest();
-    xhttp.addEventListener("load", modrinthApiResponse)
-    xhttp.open("GET", `https://api.modrinth.com/v2/project/${slug}`);
-    xhttp.send();
 }
 
 function scrollHeaderLeft() {
